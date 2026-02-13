0d84bd67eed39b6999343fa35ac7c7d05cc28d4c
Krishna <krishna.mohan@sourcemash.com>
2026-02-13 18:11:53 +0530

    updated code

--- src/app/core/interceptor-interceptor.ts
+++ src/app/core/interceptor-interceptor.ts
@@ -1,11 +1,14 @@
 import { HttpErrorResponse, HttpEvent, HttpInterceptorFn } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
-
+import {LoaderService} from '../core/loader'
+import { inject } from '@angular/core';
 export const interceptorInterceptor: HttpInterceptorFn = (req, next): Observable<HttpEvent<any>>=> {
- 
+ const servics = inject(LoaderService)
+      servics.show()
  
   return next(req).pipe(catchError((error:HttpErrorResponse)=>{
 
+    servics.hide()
     let errorMsg = "";
     if(error.error instanceof ErrorEvent){
       errorMsg = `Client Error : ${error.error.message}`
